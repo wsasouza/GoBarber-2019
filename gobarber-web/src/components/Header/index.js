2160f3f81c6cd587b9c4f5dc87d47a5495006030
Walter Souza <wsasouza@hotmail.com>
2020-05-15 12:17:20 -0300

    First Commit

---
+++ gobarber-web/src/components/Header/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import Notifications from '~/components/Notifications';
+
+import logo from '~/assets/logo-purple.svg';
+
+import { Container, Content, Profile } from './styles';
+
+import { signOut } from '~/store/modules/auth/actions';
+
+export default function Header() {
+  const profile = useSelector(state => state.user.profile);
+  const dispatch = useDispatch();
+
+  function handleSignOut() {
+    dispatch(signOut());
+  }
+
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="GoBarber" />
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+
+        <aside>
+          <Notifications />
+
+          <Profile>
+            <div>
+              <strong>{profile.name}</strong>
+              <Link to="/profile">Meu perfil</Link>
+              <button type="button" onClick={handleSignOut}>
+                Sair
+              </button>
+            </div>
+            <img
+              src={
+                profile.avatar.url ||
+                'https://api.adorable.io/avatars/50/abott@adorable.png'
+              }
+              alt="Avatar"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
